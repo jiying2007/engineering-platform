5e1e94fe9a6dff4f4eeaa122a39c7989a5c2bb9c
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-24 09:50:12 +0800

    feat: add fail-closed work lifecycle state machine

---
+++ internal/core/work_state.go
@@ -0,0 +1,29 @@
+package core
+
+import "errors"
+
+var ErrInvalidWorkTransition = errors.New("invalid work state transition")
+
+func (w *WorkItem) Transition(to WorkState) error {
+	if w.State == to {
+		return nil
+	}
+	allowed := false
+	switch w.State {
+	case WorkDraft:
+		allowed = to == WorkReady || to == WorkCancelled
+	case WorkReady:
+		allowed = to == WorkExecuting || to == WorkCancelled
+	case WorkExecuting:
+		allowed = to == WorkVerifying || to == WorkCancelled
+	case WorkVerifying:
+		allowed = to == WorkReviewing || to == WorkClosed || to == WorkCancelled
+	case WorkReviewing:
+		allowed = to == WorkClosed || to == WorkCancelled
+	}
+	if !allowed {
+		return ErrInvalidWorkTransition
+	}
+	w.State = to
+	return nil
+}
