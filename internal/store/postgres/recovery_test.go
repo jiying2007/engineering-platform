7428a0fa67d909236b6162524fab8a829e3664a5
jiying2007 <33591504+jiying2007@users.noreply.github.com>
2026-09-25 20:30:14 +0800

    test(recovery): isolate lifecycle proof state per repetition

--- internal/store/postgres/recovery_test.go
+++ internal/store/postgres/recovery_test.go
@@ -3,7 +3,6 @@ package postgres
 import (
 	"context"
 	"errors"
-	"os"
 	"testing"
 
 	"github.com/jiying2007/engineering-platform/internal/recovery"
@@ -11,24 +10,8 @@ import (
 )
 
 func TestPostgresRecoveryLifecycle(t *testing.T) {
-	url := os.Getenv("POSTGRES_TEST_URL")
-	if url == "" {
-		t.Skip("POSTGRES_TEST_URL is not set")
-	}
-
+	s := newIsolatedIntegrationStore(t)
 	ctx := context.Background()
-	s, err := Open(ctx, url)
-	if err != nil {
-		t.Fatal(err)
-	}
-	defer s.Close()
-	if err := s.ApplyCoreMigration(ctx); err != nil {
-		t.Fatal(err)
-	}
-
-	if _, err := s.pool.Exec(ctx, "UPDATE platform_state SET recovery_epoch=0,recovery_mode='NORMAL',updated_at=now() WHERE singleton_id=true"); err != nil {
-		t.Fatal(err)
-	}
 
 	state, err := s.GetRecovery()
 	if err != nil {
